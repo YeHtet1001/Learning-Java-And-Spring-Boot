1fd3c14f26032dba9110d4b19fae1e5f26695717
YeHtet1001 <java17springboot@gmail.com>
2025-04-29 21:21:03 +0630

    update

--- Synchronization-Demo/src/main/java/org/example/Main.java
+++ Synchronization-Demo/src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
         MyThreadClass thread1 = new MyThreadClass( "Hi" , "Brother" ,obj);
         MyThreadClass thread2 = new MyThreadClass( "Hello" , "Tester" ,obj);
         MyThreadClass thread3 = new MyThreadClass( "Konnichiwa" , "Developer",obj );
-        
+
     }
 }
